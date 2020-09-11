8d21cd23ac6e73beb63bed5bd6fe3fabd78d6b1a
Ryan Hutchins <rmhutchins@gmail.com>
2020-09-11 10:32:55 +0200

    initial commit of Flask tutorial project

---
+++ flaskrrmh/wsgi.py
@@ -0,0 +1,3 @@
+from flaskrrmh import create_app
+
+app = create_app()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskrrmh',
+    version='1.0.4',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": ["templates/*.html", "templates/**/*.html"],
+    },
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
